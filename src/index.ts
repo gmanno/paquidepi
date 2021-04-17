95f49a314689f4606eef6ba16f408a2105e3ef6a
Germano Carvalho <germano.carv@gmail.com>
2021-04-17 01:03:56 -0300

    feat: authorization

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,21 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express";
-import { Hash } from "node:crypto";
 
+const compression = require("compression");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+
 const saltRounds = 10;
 const salt = bcrypt.genSaltSync(saltRounds);
 
 const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
-const jwt = require("jsonwebtoken");
-
-app.post(`/login`, async (req, res) => {
-  const { email, password } = req.body;
-  const user = await prisma.user.findFirst({
-    where: {
-      email: email,
-    },
-  });
-
-  const data = { id: user?.id, name: user?.name, email: user?.email };
-  const result = await bcrypt
-    .compare(password, user?.password)
-    .then((matched: boolean) => {
-      if (matched) {
-         const token = jwt.sign(data, process.env.SECRET, {
-           expiresIn: 300, // expires in 5min
-         });
-        return { message: "Login realizado com sucesso", token };
-      } else {
-        return { message: "Senha inválida" };
-      }
-    })
-    .catch((err: any) => {
-      return { message: "E-mail não cadastrado." };
-    });
-
-  res.json(result);
-});
+app.use(compression());
 
 app.post(`/signup`, async (req, res) => {
   const { name, email, password } = req.body;
-
   const hash = bcrypt.hashSync(password, salt);
-
   await prisma.user.create({
     data: {
       name: name,
@@ -53,6 +25,80 @@ app.post(`/signup`, async (req, res) => {
   });
   const user = await prisma.user.findFirst({ where: { email: email } });
   res.json(user);
+});
+
+app.post(`/login`, async (req, res) => {
+  const { email, password } = req.body;
+  await prisma.user
+    .findFirst({
+      where: {
+        email: email,
+      },
+    })
+    .then((user) => {
+      const data = { id: user?.id, name: user?.name, email: user?.email };
+      bcrypt
+        .compare(password, user?.password)
+        .then((matched: boolean) => {
+          if (matched) {
+            const access_token = jwt.sign({ user: data }, process.env.SECRET, {
+              expiresIn: 600, // expires in 5min
+            });
+            res.json({
+              message: "Login realizado com sucesso",
+              token: { access_token },
+            });
+          } else {
+            res.status(401).json({ message: "Senha inválida" });
+          }
+        })
+        .catch(() => {
+          res.status(401).json({ message: "E-mail não cadastrado." });
+        });
+    });
+});
+
+const verifyJWT = (req: any, res: any, next: any) => {
+  try {
+    const token = req.headers["authorization"].split(" ").slice(-1).pop();
+    console.log(token);
+    if (!token)
+      return res
+        .status(401)
+        .json({ auth: false, message: "No token provided." });
+
+    jwt.verify(token, process.env.SECRET, function (err: any, decoded: any) {
+      if (err)
+        return res.status(500).json({
+          auth: false,
+          message: "Failed to authenticate token.",
+          error: err,
+        });
+
+      // se tudo estiver ok, salva no request para uso posterior
+      req.current_user = decoded.user;
+      next();
+    });
+  } catch (err) {
+    return res.status(500).json({
+      auth: false,
+      message: "Failed to authenticate token.",
+      error: err,
+    });
+  }
+};
+
+app.get(`/users`, verifyJWT, async (req, res, next) => {
+  const result = await prisma.user.findMany({
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+  res.json(result);
 });
 
 const server = app.listen(3000, () =>
