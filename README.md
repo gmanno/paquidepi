e4cea577c8603d882e668662842372cb3bf8decc
Germano Carvalho <germano.carv@gmail.com>
2021-04-19 22:15:40 -0300

    doc: readme update

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+# PAQUIDEPI
 # REST API Example
 
 This example shows how to implement a **REST API with TypeScript** using [Express](https://expressjs.com/) and [Prisma Client](https://www.prisma.io/docs/concepts/components/prisma-client). It is based on a POSTGRES database.
--- src/index.ts
+++ src/index.ts
@@ -66,5 +66,5 @@ app.use("/users", users);
 const server = app.listen(3000, () =>
   console.log(`
 🚀 Server ready at: http://localhost:3000
-⭐️ See sample requests: http://pris.ly/e/ts/rest-express#3-using-the-rest-api`)
+⭐️ Check repo at: https://github.com/gmanno/paquidepi`)
 );
